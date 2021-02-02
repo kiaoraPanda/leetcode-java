d833c6a22f21b25f959984b03386f107aec602dd
HiPanda <747623273@qq.com>
2021-02-02 09:51:33 +0800

    公平的糖果棒交换 用数组索引

--- 公平的糖果棒交换.java
+++ 公平的糖果棒交换.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class 公平的糖果棒交换 {
-    // 第一版 直接暴力解 时间复杂度为O(n^2)
+    // 第一版O(n^2) 直接暴力解
     public int[] fairCandySwapViolence(int[] A, int[] B) {
         int aSum = 0, bSum = 0, diff;
         for (int i : A) {
@@ -24,8 +24,8 @@ public class 公平的糖果棒交换 {
         return null;
     }
 
-    // 第二版 使用哈希表 快速查找。 在查找某个值的时候可以使用哈希表来减少嵌套循环
-    public int[] fairCandySwap(int[] A, int[] B) {
+    // 第二版O(n+m) 使用哈希表 快速查找。 在查找某个值的时候可以使用哈希表来减少嵌套循环
+    public int[] fairCandySwapHashtable(int[] A, int[] B) {
         int aSum = 0, bSum = 0, diff;
         Set<Integer> set = new HashSet<>();
 
@@ -37,7 +37,7 @@ public class 公平的糖果棒交换 {
         for (int i : B) {
             bSum += i;
         }
-        // 使用下面这种方式竟然比上面的那种遍历两次的花费的时间长
+        // 使用下面这种方式竟然比上面的那种遍历两次的花费的时间长 应该是少了一个n的遍历 多了2*n的判断
 //        for (int i = 0; i < Math.max(A.length, B.length); i++) {
 //            if (i < A.length) {
 //                aSum += A[i];
@@ -57,7 +57,30 @@ public class 公平的糖果棒交换 {
         }
         return null;
     }
-    // 使用数组来保留值 已知n的大小的时候可用
+
+    // 第三版O(n+m) 使用数组来保留值 已知n的大小的时候可用 速度提高 通用性降低 需要处理很多数组越界相关的问题
+    public int[] fairCandySwap(int[] A, int[] B) {
+        int aSum = 0, bSum = 0, diff;
+        boolean[] aSet = new boolean[100001];
+
+        for (int i : A) {
+            aSum += i;
+            aSet[i] = true;
+        }
+        for (int i : B) {
+            bSum += i;
+        }
+
+        diff = (bSum - aSum) / 2;
+        int need;
+        for (int i : B) {
+            need = i - diff;
+            if (need >= 0 && need < 100001 && aSet[need]) {
+                return new int[]{i - diff, i};
+            }
+        }
+        return null;
+    }
 
     public static void main(String[] args) {
 
