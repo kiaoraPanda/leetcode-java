a6760d4b501234667397db419bb9f85226cb1a49
HiPanda <747623273@qq.com>
2021-02-22 17:47:13 +0800

    回文数 // 对于数字可以用数学方法对数字进行入栈出栈

---
+++ 回文数.java
@@ -0,0 +1,18 @@
+public class 回文数 {
+    public static boolean isPalindrome(int x) {
+        if (x < 0)
+            return false;
+        int r = x;
+        int rev = 0;
+        while (r != 0) {
+            int pop = r % 10;
+            r /= 10;
+            rev = rev * 10 + pop;
+        }
+        return rev == x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+}
