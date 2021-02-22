c67e034a296c07467f974ea4cd9fe6950bd84be1
HiPanda <747623273@qq.com>
2021-02-22 17:48:52 +0800

    回文数

--- 回文数.java
+++ 回文数.java
@@ -1,7 +1,8 @@
 public class 回文数 {
     public static boolean isPalindrome(int x) {
-        if (x < 0)
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
             return false;
+        }
         int r = x;
         int rev = 0;
         while (r != 0) {
